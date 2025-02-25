93ffc4090e1b702f4e97ad93df4952d17e46ce22
TEEWEIPHENG <teeweipheng@gmail.com>
2025-02-25 23:34:18 +0800

    enhanced UI

--- src/components/common/CustomButton.js
+++ src/components/common/CustomButton.js
@@ -1,14 +1,81 @@
 import React from "react";
+import colors from "../../styles/colors";
+
+const styles = {
+    buttonContainer: {
+        width: '100%'
+    },
+    primary: {
+        fontSize: "1.0em",
+        backgroundColor: colors.warmTaupe,
+        color: colors.softBeige,
+        padding: "10px 15px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+        width: "100%",
+    }, 
+    secondary: {
+        backgroundColor: colors.oliveGreen,
+        color: colors.softBeige,
+        padding: "10px 15px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+        width: "100%",
+    },
+    tertiary: {
+        backgroundColor: colors.honeyYellow,
+        color: colors.softBeige,
+        padding: "10px 15px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+        width: "100%",
+    },
+    other: {
+        backgroundColor: colors.warmGray,
+        color: colors.softBeige,
+        padding: "10px 15px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+        width: "100%",
+    }
+}
 
 function CustomButton({
     title,
     onClick = () =>{},
+    buttonType = "primary"
 }){
-    return(
-        <div>
-            <button onClick={onClick}>{title}</button>
-        </div>
-    )
+    switch(buttonType){
+        case "primary":
+            return(
+                <div style={styles.buttonContainer}>
+                    <button style={styles.primary} onClick={onClick}>{title}</button>
+                </div>
+            );
+        case "secondary":
+            return(
+                <div style={styles.buttonContainer}>
+                    <button style={styles.secondary} onClick={onClick}>{title}</button>
+                </div>
+            );
+        case "tertiary":
+            return(
+                <div style={styles.buttonContainer}>
+                    <button style={styles.tertiary} onClick={onClick}>{title}</button>
+                </div>
+            );
+        default:
+            return(
+                <div style={styles.buttonContainer}>
+                    <button style={styles.other} onClick={onClick}>{title}</button>
+                </div>
+            );
+    }
+    
 }
 
 export default CustomButton;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -2,41 +2,66 @@ import React, { useState } from 'react';
 import { processLogin } from '../../api/login';
 import CustomInput from '../common/CustomInput';
 import CustomButton from '../common/CustomButton';
-import '../../styles/login.css';
 import CustomHyperlink from '../common/CustomHyperlink';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../../styles/login.css';
 
 function LoginPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const onClickLoginButton = () => {
-        console.log(processLogin({username, password}));
-    }
-
-    const handleChangeUsername = (event) => {
-        setUsername(event.target.value);
-    }
-
-    const handleChangePassword = (event) => {
-        if(event.target.value.length <= 10)
-            setPassword(event.target.value);
+        console.log(processLogin({ username, password }));
     }
 
     return (
-        <div className='container'>
-            <h1>CYBERTIP</h1>
-            <CustomInput 
-                title="Username" 
-                value={username} 
-                onChange={handleChangeUsername} />
-                
-            <CustomInput 
-                title="Password" 
-                value={password} 
-                onChange={handleChangePassword} />
-            <CustomButton title="Login" onClick={onClickLoginButton} />
-            <CustomHyperlink title="No Account? Register Now!" link="#"/>
-        </div>
+        <Container className="loginContainer">
+            <div className="loginCard">
+                <h1 className="loginHeader">CyberTIP</h1>
+
+                <Row>
+                    <Col>
+                        <CustomInput 
+                            title="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+
+                        <div className="forgot-link">
+                            <CustomHyperlink title="Forgot Username?" link="#" />
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <CustomInput 
+                            title="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <div className="forgot-link">
+                            <CustomHyperlink title="Forgot Password?" link="#" />
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <CustomButton class="loginBtn" title="Login" onClick={onClickLoginButton} buttonType="primary" />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="register-link">
+                        <CustomHyperlink title="No Account? Register Now!" link="#" />
+                    </Col>
+                </Row>
+            </div>
+        </Container>
     );
 }
 
